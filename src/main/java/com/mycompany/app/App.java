57f2ce93daf2860315e1cc09854434378bc27cd6
joannaz27 <joannaz27@nycstudents.net>
2024-09-13 09:24:40 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,12 +1,7 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
-package com.mycompany.app;
-
-public class App {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        System.out.print("Hello!");
+        System.out.println("How are you?");
+        
     }
 }
